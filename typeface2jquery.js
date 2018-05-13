87cf4a3caa478eb0e78296dfd92d3cbec4526436
corrp351 <corrp351@newschool.edu>
2018-05-12 23:59:09 -0400

    lab typeface

---
+++ typeface2jquery.js
@@ -0,0 +1,167 @@
+$(document).ready(function(){
+	$("#Aleft1").draggable();
+	$("#Aright1").draggable();
+	$("#Amiddle1").draggable();
+
+	$("#Bback1").draggable();
+	$("#Btop1").draggable();
+	$("#Brighttop1").draggable();
+	$("#Bmiddle1").draggable();
+	$("#Brightbottom1").draggable();
+	$("#Bbottom1").draggable();
+	$("#Bback2").draggable();
+	$("#Btop2").draggable();
+	$("#Brighttop2").draggable();
+	$("#Bmiddle2").draggable();
+	$("#Brightbottom2").draggable();
+	$("#Bbottom2").draggable();
+
+	$("#Dleft1").draggable();
+	$("#Dtop1").draggable();
+	$("#Dbottom1").draggable();
+	$("#Dright1").draggable();
+	$("#Dleft2").draggable();
+	$("#Dtop2").draggable();
+	$("#Dbottom2").draggable();
+	$("#Dright2").draggable();
+	$("#Dleft3").draggable();
+	$("#Dtop3").draggable();
+	$("#Dbottom3").draggable();
+	$("#Dright3").draggable();
+	$("#Dleft4").draggable();
+	$("#Dtop4").draggable();
+	$("#Dbottom4").draggable();
+	$("#Dright4").draggable();
+
+	$("#Eback1").draggable();
+	$("#Etop1").draggable();
+	$("#Emiddle1").draggable();
+	$("#Ebottom1").draggable();
+	$("#Eback2").draggable();
+	$("#Etop2").draggable();
+	$("#Emiddle2").draggable();
+	$("#Ebottom2").draggable();
+	$("#Eback3").draggable();
+	$("#Etop3").draggable();
+	$("#Emiddle3").draggable();
+	$("#Ebottom3").draggable();
+	$("#Eback4").draggable();
+	$("#Etop4").draggable();
+	$("#Emiddle4").draggable();
+	$("#Ebottom4").draggable();
+
+	$("#Gback1").draggable();
+	$("#Gtop1").draggable();
+	$("#Grightsmall1").draggable();
+	$("#Gmiddle1").draggable();
+	$("#Gbottom1").draggable();
+	$("#Gback2").draggable();
+	$("#Gtop2").draggable();
+	$("#Grightsmall2").draggable();
+	$("#Gmiddle2").draggable();
+	$("#Gbottom2").draggable();
+	$("#Gback3").draggable();
+	$("#Gtop3").draggable();
+	$("#Grightsmall3").draggable();
+	$("#Gmiddle3").draggable();
+	$("#Gbottom3").draggable();
+
+	$("#Hleft1").draggable();
+	$("#Hright1").draggable();
+	$("#Hmiddle1").draggable();
+	$("#Hleft2").draggable();
+	$("#Hright2").draggable();
+	$("#Hmiddle2").draggable();
+
+	$("#Istock1").draggable();
+	$("#Itop1").draggable();
+	$("#Ibottom1").draggable();
+	$("#Istock2").draggable();
+	$("#Itop2").draggable();
+	$("#Ibottom2").draggable();
+	$("#Istock3").draggable();
+	$("#Itop3").draggable();
+	$("#Ibottom3").draggable();
+	$("#Istock4").draggable();
+	$("#Itop4").draggable();
+	$("#Ibottom4").draggable();
+
+	$("#Lstock1").draggable();
+	$("#Lbottom1").draggable();
+	$("#Lstock2").draggable();
+	$("#Lbottom2").draggable();
+
+	$("#Nleft1").draggable();
+	$("#Nright1").draggable();
+	$("#Nmiddle1").draggable();
+	$("#Nleft2").draggable();
+	$("#Nright2").draggable();
+	$("#Nmiddle2").draggable();
+	$("#Nleft3").draggable();
+	$("#Nright3").draggable();
+	$("#Nmiddle3").draggable();
+
+	$("#Oleft1").draggable();
+	$("#Oright1").draggable();
+	$("#Otop1").draggable();
+	$("#Obottom1").draggable();
+	$("#Oleft2").draggable();
+	$("#Oright2").draggable();
+	$("#Otop2").draggable();
+	$("#Obottom2").draggable();
+
+	$("#Pleft1").draggable();
+	$("#Ptop1").draggable();
+	$("#Pbottom1").draggable();
+	$("#Pright1").draggable();
+
+	$("#Rleft1").draggable();
+	$("#Rtop1").draggable();
+	$("#Rbottom1").draggable();
+	$("#Rright1").draggable();
+	$("#Rslant1").draggable();
+
+	$("#Stop1").draggable();
+	$("#Sleftside1").draggable();
+	$("#Smiddle1").draggable();
+	$("#Srightside1").draggable();
+	$("#Sbottom1").draggable();
+	$("#Stop2").draggable();
+	$("#Sleftside2").draggable();
+	$("#Smiddle2").draggable();
+	$("#Srightside2").draggable();
+	$("#Sbottom2").draggable();
+
+	$("#Tstock1").draggable();
+	$("#Ttop1").draggable();
+	$("#Tstock2").draggable();
+	$("#Ttop2").draggable();
+
+	$("#Uleft1").draggable();
+	$("#Uright1").draggable();
+	$("#Ubottom1").draggable();
+	$("#Uleft2").draggable();
+	$("#Uright2").draggable();
+	$("#Ubottom2").draggable();
+	$("#Uleft3").draggable();
+	$("#Uright3").draggable();
+	$("#Ubottom3").draggable();
+	$("#Uleft4").draggable();
+	$("#Uright4").draggable();
+	$("#Ubottom4").draggable();
+
+	$("#Vleft1").draggable();
+	$("#Vright1").draggable();
+
+	$("#Wleft1").draggable();
+	$("#Wmiddleleft1").draggable();
+	$("#Wmiddleright1").draggable();
+	$("#Wright1").draggable();
+	$("#Wleft2").draggable();
+	$("#Wmiddleleft2").draggable();
+	$("#Wmiddleright2").draggable();
+	$("#Wright2").draggable();
+
+});
+
+
